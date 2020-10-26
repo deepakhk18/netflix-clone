c60cf1ef680884c2458209f91c74cab2864504a7
deepakhk18 <42630385+deepakhk18@users.noreply.github.com>
2020-10-26 14:29:33 +0530

    Add files via upload

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "a92a3be8151d3db3b9184591022d3dd8";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
+  fetchTopRated: `/movies/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movies/?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
